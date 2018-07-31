378a97342ba9434dabb71870df459c2b16f30937
amy null <null@amy.gg>
2018-07-31 16:52:44 -0400

    Actual unit tests!? What is this heresy D:<

--- lib/q.ex
+++ lib/q.ex
@@ -32,43 +32,58 @@ defmodule Q do
   require Logger
 
   def start_link(opts) do
-    GenServer.start_link __MODULE__, opts, name: opts[:name]
+    GenServer.start_link(__MODULE__, opts, name: opts[:name])
   end
 
   def init(opts) do
-    state = %{
-      queue: opts[:queue],
-      redix: Redix.start_link([
+    {:ok, redix} =
+      Redix.start_link(
         host: opts[:host],
         port: opts[:port],
-        password: opts[:pass],
-      ]),
+        password: opts[:pass]
+      )
+
+    state = %{
+      queue: opts[:queue],
+      redix: redix,
       event_handler: opts[:event_handler]
     }
-    Logger.info "[Q] Ready on queue #{state[:queue]}!"
+
+    Logger.info("[Q] Ready on queue #{state[:queue]}!")
+
     if opts[:poll] do
       Process.send_after self(), :poll, 250
-      Logger.info "[Q] Polling will start in 250ms."
+      Process.send_after self(), :poll_started, 250
+      Logger.info("[Q] Polling will start in 250ms.")
     end
+
     {:ok, state}
   end
 
+  def handle_info(:poll_started, state) do
+    Logger.info "[Q] Polling started."
+    {:noreply, state}
+  end
+
   def handle_info(:poll, state) do
-    {:ok, data} = Redix.command state[:redix], ["BLPOP", state[:queue], "0"]
+    # This can potentially block forever, so it has to have :infinity timeout
+    {:ok, data} = Redix.command state[:redix], ["BLPOP", state[:queue], "0"], timeout: :infinity
 
     # Pass data off to handler
     state[:event_handler].(data)
 
-    send self(), :poll
+    send(self(), :poll)
     {:noreply, state}
   end
 
   def handle_cast({:queue, data}, state) when is_binary(data) do
-    Redix.command state[:redix], ["RPUSH", state[:queue], data]
+    Redix.command(state[:redix], ["RPUSH", state[:queue], data])
     {:noreply, state}
   end
+
   def handle_cast({:queue, data}, state) do
-    Redix.command state[:redix], ["RPUSH", state[:queue], Jason.encode!(data)]
+    json = Jason.encode!(data)
+    Redix.command(state[:redix], ["RPUSH", state[:queue], json])
     {:noreply, state}
   end
 
--- test/q_test.exs
+++ test/q_test.exs
@@ -1,8 +1,42 @@
+defmodule Poller do
+  require Logger
+
+  def on_poll(data) do
+    Logger.info("[POLLER] Got data: #{inspect(data, pretty: true)}")
+  end
+end
+
 defmodule QTest do
   use ExUnit.Case
+  require Logger
   doctest Q
 
-  test "greets the world" do
-    assert Q.hello() == :world
+  setup do
+    {:ok, genserver} =
+      Q.start_link(%{
+        name: :q_test,
+        queue: "q:test:queue",
+        host: System.get_env("REDIS_IP"),
+        port: 6379,
+        pass: System.get_env("REDIS_PASS"),
+        event_handler: &Poller.on_poll/1,
+        poll: true
+      })
+
+    {:ok, process: genserver}
+  end
+
+  test "greets the world", %{process: process} do
+    if System.get_env("REDIS_IP") != nil and System.get_env("REDIS_PASS") != nil do
+      GenServer.cast(process, {:queue, %{"test" => true}})
+
+      GenServer.cast(process, {:queue, %{"test2" => true}})
+
+      Process.sleep(2500)
+      assert true
+    else
+      Logger.warn("[TEST] No env, not testing!")
+      assert true
+    end
   end
 end
